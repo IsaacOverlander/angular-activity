fc3d116d21ab2606ff593d29c034fd516774c72e
Unknown <Isaac.overlander@gmail.com>
2018-08-08 13:33:16 -0500

    Added a button that displays an array of the inputs

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,4 +2,11 @@ let foodApp = angular.module('foodApp', []);
 
 foodApp.controller('FoodController', function() {
     let vm =this;
+    vm.allFoods = [];
+
+    vm.addFood = function() {
+        console.log(vm.foodIn);
+        
+        vm.allFoods.push(vm.foodIn);
+    }
 })
